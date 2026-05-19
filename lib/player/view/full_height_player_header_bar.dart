c740200b0310f6b4ab317712e812250b24901351
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2026-05-19 19:11:34 +0200

    fix: podcast commands and control improvements (#1525)
    * fix: podcast commands and control improvements
    
    * fix: update leading button logic in FullHeightPlayerHeaderBar

--- lib/player/view/full_height_player_header_bar.dart
+++ lib/player/view/full_height_player_header_bar.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_it/flutter_it.dart';
+import '../../app/app_manager.dart';
 import '../../common/view/header_bar.dart';
+import '../../common/view/icons.dart';
 import '../../extensions/build_context_x.dart';
+import '../../extensions/taget_platform_x.dart';
+import '../../l10n/l10n.dart';
 import 'full_height_player_top_controls.dart';
 import 'player_view.dart';
 
@@ -17,6 +22,17 @@ class FullHeightPlayerHeaderBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return HeaderBar(
+      leading: playerPosition == PlayerPosition.sideBar || isMobile
+          ? Center(
+              child: IconButton(
+                tooltip: context.l10n.fullWindow,
+                icon: Icon(Iconz.fullScreen),
+                onPressed: () => di<AppManager>().setFullWindowMode(
+                  playerPosition == PlayerPosition.sideBar,
+                ),
+              ),
+            )
+          : null,
       adaptive: false,
       includeBackButton: false,
       includeSidebarButton: false,
@@ -31,7 +47,7 @@ class FullHeightPlayerHeaderBar extends StatelessWidget {
                     ? Colors.white
                     : context.colorScheme.onSurface,
                 playerPosition: playerPosition,
-                showFullScreenButton: isVideo,
+                video: isVideo,
               ),
             ],
     );
